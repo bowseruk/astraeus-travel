881bf3b8a08e08a21db69348760e3f29dac62dff
unknown <aniljassi88@gmail.com>
2023-03-23 20:19:41 +0000

    back up json data for planets

--- src/components/planetData.jsx
+++ src/components/planetData.jsx
@@ -5,3 +5,71 @@ function PlanetData(props) {
 }
 
 export default PlanetData;
+
+const myArray = [
+    {
+      "name": "Mercury",
+      "Type": "Terrestrial",
+      "length of year": 88,
+      "Distance from sun": 30349799,
+      "Velocity": 9507,
+      "Area of Solar System": innerSolarSystem,
+
+    },
+    {
+        "name": "Venus",
+        "Type": "Terrestrial",
+        "length of year": 225,
+        "Distance from sun": 66888973,
+        "Velocity": 78339,
+      "Area of Solar System": innerSolarSystem,
+    },
+    {
+        "name": "Earth",
+        "Type": "Terrestrial",
+        "length of year": 365,
+        "Distance from sun": 92645838,
+        "Velocity": 25031,
+      "Area of Solar System": innerSolarSystem,
+    },
+    {
+        "name": "Mars",
+      "Type": "Terrestrial",
+      "length of year": 687,
+      "Distance from sun": 152754627,
+      "Velocity": 11252,
+      "Area of Solar System": innerSolarSystem,
+    },
+    {
+        "name": "Jupiter",
+      "Type": "Gas Giant",
+      "length of year": 4333,
+      "Distance from sun": 460330331,
+      "Velocity": 134664,
+      "Area of Solar System": outerSolarSystem,
+    },
+    {
+        "name": "Saturn",
+      "Type": "Gas Giant",
+      "length of year": 10759,
+      "Distance from sun": 912331223,
+      "Velocity": 80731,
+      "Area of Solar System": outerSolarSystem,
+    },
+    {
+        "name": "Uranus",
+      "Type": "Ice Giant",
+      "length of year": 30687,
+      "Distance from sun": 1893363345,
+      "Velocity": 47826,
+      "Area of Solar System": outerSolarSystem,
+    },
+    {
+        "name": "Pluto",
+      "Type": "Dwarf Planet",
+      "length of year": 90530,
+      "Distance from sun": 3228582263,
+      "Velocity": 52702,
+      "Area of Solar System": outerSolarSystem,
+    }
+  ];
