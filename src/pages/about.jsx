eb1832d89dac4deb9d75757b08cc4d144a05f2d9
ChrisCode3 <malusdarkblade66@gmail.com>
2023-03-28 16:00:46 +0100

    first draft of about page - layout and images

---
+++ src/pages/about.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "./about.css"
+
+function About() {
+    return (
+        <section className="section">
+            <header >About page</header>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <div className="container5">
+               <figure><img src="/src/assets/images/hs-2015-29-a-xlarge_web.jpg " width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Christos Laios</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+          
+
+            <div className="container5">
+                <figure><img src="/src/assets/images/jupiterjpg.jpg" width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Claire Ingram</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container5">
+                <figure><img src="/src/assets/images/blackhole.png" width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Adam Bolsover</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container5">
+                <figure><img src="/src/assets/images/stsci-h-p1943a-f-2076x1484 (1).png" width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Daisy Everard</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+            <div className="container5">
+                <figure><img src="/src/assets/images/jpegPIA04921.jpg"  width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Angelo Silva</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+            <div className="container5">
+                <figure><img src="/src/assets/images/solar-flare.en.jpg" width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Clarissa Cole</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+            <div className="container5">
+                <figure><img src="/src/assets/images/PIA04924_small.jpg"  width="500" height="500" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Anil Jassi</h2>
+                    <p>Insert space quote</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">GitHub</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </section>
+    );
+}
+
+export default About;
