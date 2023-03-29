2b59b8be3c91051013911189eb3995b6296e321e
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-29 21:09:47 +0100

    fixed missing divs in merge

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -50,58 +50,60 @@ function About() {
                     </div>
                 </div>
 
-            <div className="container5">
-                <figure><img src= {image3} width="1000" height="750" alt="Black hole" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title"><em>Adam Bolsover</em></h2>
-                    <p> <em>"The only way to discover the limits of the possible is to go beyond them into the impossible." - Arthur C. Clarke
-                    </em> </p>
-                    <div className="card-actions justify-end">
-                        <a href="https://github.com/bowseruk" target="_blank" >
-                            <button className="btn btn-primary">GitHub</button>
-                        </a>
-
-                    </div>
-                </div>
-
                 <div className="container5">
-                    <figure><img src={image4} width="1000" height="750" alt="Saturn" /></figure>
+                    <figure><img src={image3} width="1000" height="750" alt="Black hole" /></figure>
                     <div className="card-body">
-                        <h2 className="card-title"><em>Daisy Everard</em></h2>
-                        <p> <em>“monoculture, turned inward in an everlasting round of tedium.  <br></br>Only by accepting the other can it truly find diversion and inspiration; <br></br> only by allowing the universe to be separate from it can it have the infinite variety it craves.” <br></br> ― Adrian Tchaikovsky, Children of Ruin
+                        <h2 className="card-title"><em>Adam Bolsover</em></h2>
+                        <p> <em>"The only way to discover the limits of the possible is to go beyond them into the impossible." - Arthur C. Clarke
                         </em> </p>
                         <div className="card-actions justify-end">
-                            <a href="https://github.com/DaisyEverard" target="_blank" >
+                            <a href="https://github.com/bowseruk" target="_blank" >
                                 <button className="btn btn-primary">GitHub</button>
                             </a>
+
                         </div>
                     </div>
-                </div>
 
-            <div className="container5">
-                <figure><img src={image6} width="1000" height="750" alt="Solar Flare" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title"><em>Clarissa Cole</em></h2>
-                    <p> <em>so much universe so little time </em>
-                    </p>
-                    <div className="card-actions justify-end">
-                        <a href="https://github.com/RissaC2" target="_blank" >
-                            <button className="btn btn-primary">GitHub</button>
-                        </a>
-
+                    <div className="container5">
+                        <figure><img src={image4} width="1000" height="750" alt="Saturn" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title"><em>Daisy Everard</em></h2>
+                            <p> <em>“monoculture, turned inward in an everlasting round of tedium.  <br></br>Only by accepting the other can it truly find diversion and inspiration; <br></br> only by allowing the universe to be separate from it can it have the infinite variety it craves.” <br></br> ― Adrian Tchaikovsky, Children of Ruin
+                            </em> </p>
+                            <div className="card-actions justify-end">
+                                <a href="https://github.com/DaisyEverard" target="_blank" >
+                                    <button className="btn btn-primary">GitHub</button>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-                <div className="container5">
-                    <figure><img src={image5} width="1000" height="750" alt="Nebula" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title"><em>Anil Jassi</em></h2>
-                        <p> <em>Modern science says: 'The sun is the past, the earth is the present, the moon is the future.'<br></br> From an incandescent mass we have originated, and into a frozen mass we shall turn. <br></br> Merciless is the law of nature, and rapidly and irresistibly we are drawn to our doom. <br></br> ― Nikola Tesla
-                        </em> </p>
-                        <div className="card-actions justify-end">
-                            <a href="https://github.com/Skyline199" target="_blank" >
-                                <button className="btn btn-primary">GitHub</button>
-                            </a>
+                    <div className="container5">
+                        <figure><img src={image6} width="1000" height="750" alt="Solar Flare" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title"><em>Clarissa Cole</em></h2>
+                            <p> <em>so much universe so little time </em>
+                            </p>
+                            <div className="card-actions justify-end">
+                                <a href="https://github.com/RissaC2" target="_blank" >
+                                    <button className="btn btn-primary">GitHub</button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="container5">
+                        <figure><img src={image5} width="1000" height="750" alt="Nebula" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title"><em>Anil Jassi</em></h2>
+                            <p> <em>Modern science says: 'The sun is the past, the earth is the present, the moon is the future.'<br></br> From an incandescent mass we have originated, and into a frozen mass we shall turn. <br></br> Merciless is the law of nature, and rapidly and irresistibly we are drawn to our doom. <br></br> ― Nikola Tesla
+                            </em> </p>
+                            <div className="card-actions justify-end">
+                                <a href="https://github.com/Skyline199" target="_blank" >
+                                    <button className="btn btn-primary">GitHub</button>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
