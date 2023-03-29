779b94dfe1192d04360f4a838f089826bd546c6b
DaisyEverard <msdaisyeverard@gmail.com>
2023-03-29 18:58:19 +0100

    update base sizing for sizeComparison.jsx

--- src/components/sizeComparison.jsx
+++ src/components/sizeComparison.jsx
@@ -4,16 +4,23 @@ import planetJson from "./planetDescription.json"
 
 function SizeComparison(props) {
      const planetObj = planetJson.filter(pnt => pnt.name === props.planet)[0]
-     const diameterRatio = (planetObj.Diameter/12742) * 20 + "vw"
+     const diameterRatio = (planetObj.Diameter/12742) * 150 + "px"
      console.log("diameter: " + diameterRatio)
+
+     const earthStyle = {
+        width: "150px", height: "150px"
+     }
+     const compStyle = {
+        width: diameterRatio, height: diameterRatio
+     }
 
     return <div id='sizeComparison'>
         <h2>SIZE OF {props.planet.toUpperCase()}</h2>
-        <div className='earth' style={{width: "20vw", height: "20vw"}}>
+        <div className='earth' style={earthStyle}>
             <p>Earth</p>
         </div>
         <div className="compPlanet" 
-      style={{width: diameterRatio, height: diameterRatio}}>
+      style={compStyle}>
         </div>
     </div>
 }
