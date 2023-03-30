27971c31f9143ba6010bf1be83aa03212fb8dc93
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-30 15:05:13 +0100

    Improved social button

--- src/components/aboutCard.jsx
+++ src/components/aboutCard.jsx
@@ -8,14 +8,24 @@ function AboutCard(props) {
     return (
         <div className="team-card card">
             <img className="card-img-top" src={AboutPictureLoader(props.picIndex)} alt={props.fullName + " chosen avatar."} />
-            <div className="card-body">
+            <div className="card-body d-flex flex-column justify-content-between">
+                <div>
                 <h5 className="card-title">{props.fullName}</h5>
                 <p className="quote-text card-text">"{props.quote}"</p>
-                <div className="card-spacer"></div>
                 <p className="attribute-text card-text">{props.quoteAttributed}</p>
-                <a href={props.github} className="btn btn-dark">
+                </div>
+                <div class="card-spacer"></div>
+                <div className="card-social d-flex justify-content-around">
+                { props.github && <a href={props.github} className="btn btn-dark">
                     <i className="fa-brands fa-github" />
-                </a>
+                </a> }
+                { props.linkedin && <a href={props.linkedin} className="btn btn-primary">
+                    <i className="fa-brands fa-linkedin" />
+                </a> }
+                { props.twitter && <a href={props.twitter} className="btn btn-primary">
+                    <i className="fa-brands fa-twitter" />
+                </a> }
+                </div>
             </div>
         </div>
     )
