d702e098d3c1f431636f25143b86fdd86aeaab4d
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-03-30 02:13:15 +0100

    Fix for the navigation class error

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -13,9 +13,9 @@ function Navigation() {
       <Link to="/"><img className="logo" src={logo} alt="Logo" /></Link>
       <nav>
         <ul>
-          <li className="navLink"><Link to="/"><i class="fa-solid fa-house"></i> <span>Home</span></Link></li>
-          <li className="navLink"><Link to={randomPage()}><i class="fa-solid fa-shuffle"></i> <span>Random</span></Link></li>
-          <li className="navLink"><Link to="/About"><i class="fa-solid fa-question"></i> <span>About</span></Link></li>
+          <li className="navLink"><Link to="/"><i className="fa-solid fa-house"></i> <span>Home</span></Link></li>
+          <li className="navLink"><Link to={randomPage()}><i className="fa-solid fa-shuffle"></i> <span>Random</span></Link></li>
+          <li className="navLink"><Link to="/About"><i className="fa-solid fa-question"></i> <span>About</span></Link></li>
         </ul>
       </nav>
     </header>
